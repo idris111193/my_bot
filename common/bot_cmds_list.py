5ea9d7fbea592849b15bd72ee0467eef8fa6b3e4
idris <idrisbagi93@gmail.com>
2024-05-06 22:20:02 +0600

    'myBot'

--- common/bot_cmds_list.py
+++ common/bot_cmds_list.py
@@ -2,7 +2,7 @@ from aiogram.types import BotCommand
 
 private = [
     BotCommand(command='menu', description='посмотреть меню'),
-    BotCommand(command='about', description=' о нас'),
+    BotCommand(command='about', description='о магазине'),
     BotCommand(command='payment', description='варианты оплаты'),
     BotCommand(command='shipping', description='варианты доставки'),
 ]
